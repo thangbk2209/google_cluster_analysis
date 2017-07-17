57c5556ff317bffcba465fd87aa91890d9d7dbfe
thangbk2209 <thangbk2209@gmail.com>
2017-07-17 22:42:32 +0700

    add count.py

--- count.py
+++ count.py
@@ -1,7 +1,8 @@
 import pandas as pd
-
-
-startTimeDf = pd.read_csv('/mnt/volume/ggcluster/spark-2.1.1-bin-hadoop2.7/thangbk2209/TopJobId/TopJobId-startTime.csv', header=None)
-# split dataset
-startTimeArr = startTimeDf.values
-print len(startTimeArr)
+import os
+folder_path='/mnt/volume/ggcluster/spark-2.1.1-bin-hadoop2.7/thangbk2209/TopJobId/'
+for file_name in os.listdir(folder_path):
+	startTimeDf = pd.read_csv('%s'%(file_name), header=None)
+	# split dataset
+	startTimeArr = startTimeDf.values
+	print len(startTimeArr)
