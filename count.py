d59530cbeedf04aca8c24558b28b02190cebc29b
thangbk2209 <thangbk2209@gmail.com>
2017-07-18 13:20:01 +0700

    add the last part

--- count.py
+++ count.py
@@ -2,9 +2,11 @@ import pandas as pd
 import os
 
 folder_path = '/mnt/volume/ggcluster/spark-2.1.1-bin-hadoop2.7/thangbk2209/TopJobId/'
-
-for file_name in os.listdir(folder_path):
+s=0;
+for num in range(175,270):
+	file_name = "JobMaxTaskpart-00"+str(num).zfill(3)+"-of-00500.csv"
 	startTimeDf = pd.read_csv('%s%s'%(folder_path,file_name), header=None)
 	# split dataset
 	startTimeArr = startTimeDf.values
-	print len(startTimeArr)
+	s+=len(startTimeArr)
+print s
