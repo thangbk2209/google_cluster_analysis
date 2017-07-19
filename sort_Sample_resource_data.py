8c381a78a72045b1dc3450a48e3fcf0a041db52f
thangbk2209 <thangbk2209@gmail.com>
2017-07-19 11:52:02 +0700

    sort data by timeStamp

---
+++ sort_Sample_resource_data.py
@@ -0,0 +1,42 @@
+
+from pyspark.sql.session import SparkSession as spark
+import pandas as pd
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from matplotlib import pyplot
+import os
+sc = SparkContext(appName="Task_usage")
+sql_context = SQLContext(sc)
+
+# folder_path ='/mnt/volume/ggcluster/clusterdata-2011-2/task_usage/'
+folder_path = '/mnt/volume/ggcluster/spark-2.1.1-bin-hadoop2.7/thangbk2209/plotTimeSeries/'
+
+dataSchema = StructType([
+                         StructField('JobId', LongType(), True),
+                         StructField('meanCPUUsage', FloatType(), True),
+                         # canonical memory usage
+                         StructField('CMU', FloatType(), True),
+                         # assigned memory usage
+                         StructField('AssignMem', FloatType(), True),
+                         # unmapped page cache memory usage
+                         StructField('unmapped_cache_usage', FloatType(), True),
+                         StructField('page_cache_usage', FloatType(), True),
+                         StructField('mean_diskIO_time', FloatType(), True),
+                         StructField('mean_local_disk_space', FloatType(), True),
+                         StructField('timeStamp', StringType(), True)])
+
+file_name = "sample_resource_usage_TopJobId.csv"
+df = (
+    sql_context.read
+    .format('com.databricks.spark.csv')
+    .schema(dataSchema)
+    .load("%s%s"%(folder_path,file_name))
+)
+df.createOrReplaceTempView("dataFrame")
+
+DataDf = sql_context.sql("SELECT * from dataFrame order by timeStamp ASC")
+print "TimeDf.count()= "
+print TimeDf.count()
+DataDf.toPandas().to_csv('thangbk2209/plotTimeSeries/offical_data_resource_TopJobId.csv', index=False, header=None)
+sc.stop()
