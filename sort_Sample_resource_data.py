0e764e6f56b5f2f277279ef74f10681d01dc9fef
thangbk2209 <thangbk2209@gmail.com>
2017-08-01 22:24:15 +0700

    sort time stamp

--- sort_Sample_resource_data.py
+++ sort_Sample_resource_data.py
@@ -14,7 +14,9 @@ sql_context = SQLContext(sc)
 folder_path = '/mnt/volume/ggcluster/spark-2.1.1-bin-hadoop2.7/thangbk2209/plotTimeSeries/'
 
 dataSchema = StructType([
-                         # StructField('JobId', LongType(), True),
+                         StructField('JobId', LongType(), True),
+                         StructField('taskIndex', LongType(), True),
+                         StructField('machineId', LongType(), True),
                          StructField('meanCPUUsage', FloatType(), True),
                          # canonical memory usage
                          StructField('CMU', FloatType(), True),
@@ -23,8 +25,18 @@ dataSchema = StructType([
                          # unmapped page cache memory usage
                          StructField('unmapped_cache_usage', FloatType(), True),
                          StructField('page_cache_usage', FloatType(), True),
+                         StructField('max_mem_usage', FloatType(), True),
+                         StructField('mean_diskIO_time', FloatType(), True),
                          StructField('mean_local_disk_space', FloatType(), True),
+                         StructField('max_cpu_usage', FloatType(), True),
+                         StructField('max_disk_io_time', FloatType(), True),
+                         StructField('cpi', FloatType(), True),
+                         StructField('mai', FloatType(), True),
+                         StructField('sampling_portion', FloatType(), True),
+                         StructField('agg_type', FloatType(), True),
+                         StructField('sampled_cpu_usage', FloatType(), True),
                          StructField('time_stamp', LongType(), True)])
+
 file_name = "offical_data_resource_TopJobId.csv"
 df = (
     sql_context.read
@@ -34,8 +46,8 @@ df = (
 )
 df.createOrReplaceTempView("dataFrame")
 
-DataDf = sql_context.sql("SELECT  * from dataFrame order by time_stamp ASC")
+DataDf = sql_context.sql("SELECT time_stamp,sum(taskIndex),sum(machineId),sum(meanCPUUsage),sum(CMU),sum(AssignMem),sum(unmapped_cache_usage),sum(page_cache_usage), sum(max_mem_usage),sum(mean_diskIO_time),sum(mean_local_disk_space),sum(max_cpu_usage), sum(max_disk_io_time), sum(cpi), sum(mai),sum(sampling_portion),sum(agg_type),sum(sampled_cpu_usage) from dataFrame group by time_stamp order by time_stamp ASC")
 print "DataDf.count()= "
 print DataDf.count()
-DataDf.toPandas().to_csv('thangbk2209/plotTimeSeries/my_offical_data_resource_TopJobId.csv', index=False, header=None)
+DataDf.toPandas().to_csv('thangbk2209/plotTimeSeries/data_resource_JobId_6336594489.csv', index=False, header=None)
 sc.stop()
