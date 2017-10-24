0c04b67ceef1ff7c7d860610ccf71428791ef307
thangbk2209 <thangbk2209@gmail.com>
2017-10-24 12:26:06 +0700

    sort data 5 minutes

--- sort_Sample_resource_data.py
+++ sort_Sample_resource_data.py
@@ -10,8 +10,8 @@ import os
 sc = SparkContext(appName="Task_usage")
 sql_context = SQLContext(sc)
 
-folder_path ='/home/nguyen/spark-lab/spark-2.1.1-bin-hadoop2.7/google_cluster_analysis/results/'
-# folder_path = '/mnt/volume/ggcluster/spark-2.1.1-bin-hadoop2.7/thangbk2209/plotTimeSeries/results/'
+# folder_path ='/home/nguyen/spark-lab/spark-2.1.1-bin-hadoop2.7/google_cluster_analysis/results/'
+folder_path = '/mnt/volume/ggcluster/spark-2.1.1-bin-hadoop2.7/thangbk2209/plotTimeSeries/results/'
 
 dataSchema = StructType([
                          StructField('JobId', LongType(), True),
@@ -37,7 +37,7 @@ dataSchema = StructType([
                          StructField('sampled_cpu_usage', FloatType(), True),
                          StructField('time_stamp', FloatType(), True)])
 
-file_name = "extractedData.csv"
+file_name = "all_sample_resource_usage_TopJobId_5minutes.csv"
 df = (
     sql_context.read
     .format('com.databricks.spark.csv')
@@ -49,5 +49,5 @@ df.createOrReplaceTempView("dataFrame")
 DataDf = sql_context.sql("SELECT time_stamp,sum(taskIndex),sum(machineId),sum(meanCPUUsage),sum(CMU),sum(AssignMem),sum(unmapped_cache_usage),sum(page_cache_usage), sum(max_mem_usage),sum(mean_diskIO_time),sum(mean_local_disk_space),sum(max_cpu_usage), sum(max_disk_io_time), sum(cpi), sum(mai),sum(sampling_portion),sum(agg_type),sum(sampled_cpu_usage) from dataFrame group by time_stamp order by time_stamp ASC")
 print "DataDf.count()= "
 print DataDf.count()
-DataDf.toPandas().to_csv('google_cluster_analysis/results/data_resource_JobId_6336594489.csv', index=False, header=None)
+DataDf.toPandas().to_csv('/mnt/volume/ggcluster/spark-2.1.1-bin-hadoop2.7/thangbk2209/plotTimeSeries/results/data_resource_JobId_6336594489_5minutes.csv', index=False, header=None)
 sc.stop()
